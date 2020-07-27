a341dc68c05f197b0be5a74ef6bddaabc4ae6c78
BakaAless <44125445+BakaAless@users.noreply.github.com>
2020-07-27 17:28:22 +0200

    Update InventoryGUI.java

--- InventoryGUI.java
+++ InventoryGUI.java
@@ -1,4 +1,5 @@
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
@@ -18,7 +19,7 @@ public class InventoryGUI implements Listener {
     private JavaPlugin plugin;
     private Inventory inventory;
     private Map<Integer, ItemStack> slots;
-    private Map<Integer, Consumer<ItemStack>> actions;
+    private Map<Integer, Consumer<InventoryClickEvent>> actions;
     private Map<Integer, Boolean> cancelled;
 
     private InventoryGUI(final JavaPlugin plugin, final int rows, final String title) {
@@ -38,27 +39,32 @@ public class InventoryGUI implements Listener {
     }
 
     public InventoryGUI addItem(final int slot, final ItemStack itemStack) {
-        return this.addItem(slot, itemStack, itemStack1 -> {}, true);
+        return this.addItem(slot, itemStack, event -> {}, true);
     }
 
     public InventoryGUI addItem(final int slot, final ItemStack itemStack, final boolean cancelled) {
-        return this.addItem(slot, itemStack, itemStack1 -> {}, cancelled);
+        return this.addItem(slot, itemStack, event -> {}, cancelled);
     }
 
-    public InventoryGUI addItem(final int slot, final ItemStack itemStack, final Consumer<ItemStack> itemStackConsumer) {
-        return this.addItem(slot, itemStack, itemStackConsumer, true);
+    public InventoryGUI addItem(final int slot, final ItemStack itemStack, final Consumer<InventoryClickEvent> eventConsumer) {
+        return this.addItem(slot, itemStack, eventConsumer, true);
     }
 
-    public InventoryGUI addItem(final int slot, final ItemStack itemStack, final Consumer<ItemStack> itemStackConsumer, final boolean cancelled) {
+    public InventoryGUI addItem(final int slot, final ItemStack itemStack, final Consumer<InventoryClickEvent> eventConsumer, final boolean cancelled) {
         if (this.slots.containsKey(slot)) {
             this.slots.remove(slot);
             this.cancelled.remove(slot);
             this.actions.remove(slot);
         }
+        if (itemStack.getType().equals(Material.AIR)) return this;
         this.slots.put(slot, itemStack);
         this.cancelled.put(slot, cancelled);
-        this.actions.put(slot, itemStackConsumer);
+        this.actions.put(slot, eventConsumer);
         return this;
+    }
+    
+    public ItemStack getItemStack(final int slot) {
+        return this.slots.get(slot);
     }
 
     public void create(final Player player) {
@@ -73,7 +79,7 @@ public class InventoryGUI implements Listener {
         if (e.getCurrentItem() == null) return;
         if (this.slots.containsKey(e.getSlot())) {
             e.setCancelled(this.cancelled.get(e.getSlot()));
-            this.actions.get(e.getSlot()).accept(this.slots.get(e.getSlot()));
+            this.actions.get(e.getSlot()).accept(e);
         }
     }
 
